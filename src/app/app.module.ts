a9bbdc273c0cbf0e04314926264907ae309cf123
Miro <m.mihok1@gmail.com>
2023-10-31 10:28:44 +0100

    added navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AboutComponent } from './pages/about/about.component';
 import { AlumniComponent } from './pages/alumni/alumni.component';
 import { AlumnidetailsComponent } from './pages/alumni/alumnidetails/alumnidetails.component';
 import { EventsDetailsComponent } from './pages/events/events-details/events-details.component';
+import { NavbarComponent } from './Layout/navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { EventsDetailsComponent } from './pages/events/events-details/events-det
     FooterComponent,
     AlumniComponent,
     AlumnidetailsComponent,
-    EventsDetailsComponent
+    EventsDetailsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,

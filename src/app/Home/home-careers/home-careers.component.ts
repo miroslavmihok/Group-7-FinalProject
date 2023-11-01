ba4233009426f87fb20e67ad133855b960da9169
Miro <m.mihok1@gmail.com>
2023-11-01 22:12:50 +0100

    added home-events and home-careers, fixed btncolor

--- src/app/Home/home-careers/home-careers.component.ts
+++ src/app/Home/home-careers/home-careers.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { jobPositions } from 'src/app/data/careers';
+import { Icareers } from 'src/app/interfaces/icareers';
 
 @Component({
   selector: 'app-home-careers',
   templateUrl: './home-careers.component.html',
   styleUrls: ['./home-careers.component.scss']
 })
-export class HomeCareersComponent {
-
+export class HomeCareersComponent implements OnInit {
+  careers: Icareers[] = jobPositions;
+  
+  ngOnInit(): void {
+    this.careers = jobPositions.slice(0, 2);
+  }
 }
